51bac6e41729b756f4cab05e9f47b9fa0bffb644
Alastair Scott <alastairscott7@gmail.com>
2020-11-21 19:26:01 -0800

    End of godot tutorial series

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,9 +1,12 @@
 extends KinematicBody2D
 
+const PlayerHurtSound = preload("res://Player/PlayerHurtSound.tscn")
+
 # fix these values, maybe get rid of delta
 const FRICTION = 500
 const ACCELERATION = 500
 const MAX_SPEED = 80
+const ROLL_SPEED = 120
 
 enum {
 	MOVE,
@@ -13,22 +16,33 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.DOWN
+var stats = PlayerStats
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox =  $HitboxPivot/SwordHitbox
+onready var hurtbox = $Hurtbox
+onready var blinkAnimationPlayer = $BlinkAnimationPlayer
 
 func _ready():
+	randomize()
+	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
+#use _physics_process if you need access to certain physics variables
 func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
+			
 		ROLL:
-			pass
+			roll_state()
+			
 		ATTACK:
-			attack_state(delta)
+			attack_state()
 
 func move_state(delta):
 	var input_vector = Vector2.ZERO
@@ -37,24 +51,56 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	#pass in linear velocity, what is a linear velocity?
-	velocity = move_and_slide(velocity)
+	move()
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 
-func attack_state(delta):
+func roll_state():
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+
+func attack_state():
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
+	
+func move():
+	#pass in linear velocity, what is a linear velocity?
+	velocity = move_and_slide(velocity)
+
+func roll_animation_finished():
+	state = MOVE
 
 func attack_animation_finished():
 	state = MOVE
+
+func _on_Hurtbox_area_entered(area):
+	stats.health -= area.damage
+	hurtbox.start_invincibility(0.6)
+	hurtbox.create_hit_effect()
+	var playerHurtSound = PlayerHurtSound.instance()
+	get_tree().current_scene.add_child(playerHurtSound)
+
+
+func _on_Hurtbox_invincibility_started():
+	blinkAnimationPlayer.play("Start")
+
+
+func _on_Hurtbox_invincibility_ended():
+	blinkAnimationPlayer.play("Stop")
