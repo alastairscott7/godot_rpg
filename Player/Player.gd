ef03250f13434edb3457bd79d25ed29298a6f941
Alastair Scott <alastairscott7@gmail.com>
2020-12-08 19:08:24 -0800

    swap out assets with some of my own

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,11 +6,9 @@ const PlayerHurtSound = preload("res://Player/PlayerHurtSound.tscn")
 const FRICTION = 500
 const ACCELERATION = 500
 const MAX_SPEED = 80
-const ROLL_SPEED = 120
 
 enum {
 	MOVE,
-	ROLL,
 	ATTACK
 }
 
@@ -19,17 +17,21 @@ var velocity = Vector2.ZERO
 var roll_vector = Vector2.DOWN
 var stats = PlayerStats
 
-onready var animationPlayer = $AnimationPlayer
-onready var animationTree = $AnimationTree
-onready var animationState = animationTree.get("parameters/playback")
-onready var swordHitbox =  $HitboxPivot/SwordHitbox
+onready var animatedSprite = $AnimatedSprite
+#onready var animationPlayer = $AnimationPlayer
+#onready var animationTree = $AnimationTree
+#onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $HitboxPivot/SwordHitbox
+onready var swordCollisionShape = $HitboxPivot/SwordHitbox/CollisionShape2D
+onready var hitboxPivot = $HitboxPivot
 onready var hurtbox = $Hurtbox
 onready var blinkAnimationPlayer = $BlinkAnimationPlayer
+onready var lightSource = $Light
 
 func _ready():
 	randomize()
 	stats.connect("no_health", self, "queue_free")
-	animationTree.active = true
+	#animationTree.active = true
 	swordHitbox.knockback_vector = roll_vector
 
 #use _physics_process if you need access to certain physics variables
@@ -37,9 +39,6 @@ func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
-			
-		ROLL:
-			roll_state()
 			
 		ATTACK:
 			attack_state()
@@ -53,49 +52,46 @@ func move_state(delta):
 	if input_vector != Vector2.ZERO:
 		roll_vector = input_vector
 		swordHitbox.knockback_vector = input_vector
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Run/blend_position", input_vector)
-		animationTree.set("parameters/Attack/blend_position", input_vector)
-		animationTree.set("parameters/Roll/blend_position", input_vector)
-		animationState.travel("Run")
+		#animationTree.set("parameters/Idle/blend_position", input_vector)
+		#animationTree.set("parameters/Run/blend_position", input_vector)
+		#animationTree.set("parameters/Attack/blend_position", input_vector)
+		animatedSprite.play("Walk")
+		lightSource.offset.y = input_vector.y * 40
+		lightSource.offset.x = input_vector.x * 40
+		#animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		if velocity.x != 0:
+			animatedSprite.flip_h = velocity.x > 0
+			if animatedSprite.flip_h:
+				hitboxPivot.rotation_degrees = 0 
+			else:
+				hitboxPivot.rotation_degrees = -180
 	else:
-		animationState.travel("Idle")
+		#animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		animatedSprite.play("Idle")
 	
 	move()
-	
-	if Input.is_action_just_pressed("roll"):
-		state = ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 
-func roll_state():
-	velocity = roll_vector * ROLL_SPEED
-	animationState.travel("Roll")
-	move()
-
 func attack_state():
 	velocity = Vector2.ZERO
-	animationState.travel("Attack")
+	animatedSprite.play("Attack")
+	swordCollisionShape.disabled = false
+	#animationState.travel("Attack")
 	
 func move():
 	#pass in linear velocity, what is a linear velocity?
 	velocity = move_and_slide(velocity)
 
-func roll_animation_finished():
-	state = MOVE
-
-func attack_animation_finished():
-	state = MOVE
-
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	hurtbox.start_invincibility(0.6)
 	hurtbox.create_hit_effect()
-	var playerHurtSound = PlayerHurtSound.instance()
-	get_tree().current_scene.add_child(playerHurtSound)
+	#var playerHurtSound = PlayerHurtSound.instance()
+	#get_tree().current_scene.add_child(playerHurtSound)
 
 
 func _on_Hurtbox_invincibility_started():
@@ -104,3 +100,8 @@ func _on_Hurtbox_invincibility_started():
 
 func _on_Hurtbox_invincibility_ended():
 	blinkAnimationPlayer.play("Stop")
+
+
+func _on_AnimatedSprite_animation_finished():
+	swordCollisionShape.disabled = true
+	state = MOVE
--- WhiteColor.shader
+++ WhiteColor.shader
@@ -4,7 +4,7 @@ uniform bool active = false;
 
 void fragment() {
 	vec4 previous_color = texture(TEXTURE, UV);
-	vec4 white_color = vec4(1.0, 1.0, 1.0, previous_color.a);
+	vec4 white_color = vec4(1.0, 0.0, 0.0, previous_color.a);
 	vec4 new_color = previous_color;
 	if (active == true) {
 		new_color = white_color;
